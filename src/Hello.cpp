60d54b69c5e6bf50d9daea66270e4ac069fc76e6
aonghus <aonghuslawlor@gmail.com>
2016-09-18 18:42:18 +0100

    initial commit

---
+++ src/Hello.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Aonghus Lawlor
+// Version     : 1.0
+// Copyright   : All Rights Reserved
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello COMP20010" << endl;
+	return 0;
+}
